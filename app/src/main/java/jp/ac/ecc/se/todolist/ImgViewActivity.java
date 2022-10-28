b514753de885d2f3cd710306fc9bcaddd473e59b
fuchiue <fuchiue2220235@gmail.com>
2022-10-28 15:31:50 +0900

    ToDoList ファイル作成

---
+++ app/src/main/java/jp/ac/ecc/se/todolist/ImgViewActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.ecc.se.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImgViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img_view);
+    }
+}
