433d2c82e07b7bdeff58b10f73d1ee2d7d82c432
fuchiue <fuchiue2220235@gmail.com>
2022-11-04 16:49:43 +0900

    INPUTから取得したtitleをリストに表示しPreferencesに保存する。 別classからPreferencesを操作するように変更予定

--- app/src/main/java/jp/ac/ecc/se/todolist/ToDoListActivity.java
+++ app/src/main/java/jp/ac/ecc/se/todolist/ToDoListActivity.java
@@ -2,28 +2,61 @@ package jp.ac.ecc.se.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ToDoListActivity extends AppCompatActivity {
 
+    @SuppressLint("MutatingSharedPrefs")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+        //コンテキスト宣言
+        Context context = getApplicationContext();
+        //Preference宣言
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        //inPutからのintent取得
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
         //画面上のパーツ生成
         ListView ListView = findViewById(R.id.ListView);
         Button newListButton = findViewById(R.id.newListButton);
-        ArrayList<String> dataList = new ArrayList<>();
+        ArrayList<String> dataList;
+        //葉入れるの
+        Set<String> sArray = pref.getStringSet("ToDoTitle", null );
+        if ( sArray != null ) dataList = new ArrayList<String>( sArray );
+        else dataList = new ArrayList<>();
         //配列セット用のアダプタ宣言
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,dataList);
         ListView.setAdapter(adapter);
+
+        if(title!=null){
+            //Titleを取得していたらリストに追加
+            dataList.add(title);
+            //リストを保存する
+            sArray = new HashSet<String>(dataList);
+            pref.edit().putStringSet("ToDoTitle", null ).apply();
+            pref.edit().putStringSet("ToDoTitle", sArray ).apply();
+            sArray.clear();
+        }
+
+        //リストを表示
+        ListView.setAdapter(adapter);
+
 
         //新規作成ボタンが押されたとき
         newListButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +67,7 @@ public class ToDoListActivity extends AppCompatActivity {
                 startActivity(intentInp);
             }
         });
+
 
     }
 }
